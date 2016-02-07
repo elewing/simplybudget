b343f9e37816ffa6cf6336184ccf37c46aa424b8
Zainab Aqdas Rizvi <srizvi@smith.edu>
2016-02-06 19:44:08 -0500

    Parser almost done

--- parser.js
+++ parser.js
@@ -1,20 +1,52 @@
 
+dictionary = ["SPORTING", "ElECTRONIC", "WEAR", "INNS", "FOOD",
+              "RESTAURANTS", "EATING", "RENTAL", "STATIONS", "CREDIT",
+            "COMPUTER", "ELECTRICAL", "RECORD"]
+
 function xmlParser(xml) {
   var category = xml.getElementsbyTagName("MerchantCategory");
   var string = category[0].toString;
   var list = string.split(" ");
   var merchant;
-  for (i = 0; i < list.length; i++) {
-    if (typeof list[0] !== Number) {
-      merchant.push(list[0]);
-    }
+  var group;
+  for (i = 2; i < list.length; i++) {
+    merchant.push(list[i].toString);
   }
   var string = "";
   for (i = 0; i < merchant.length; i++) {
-    txt += merchant[i] + " ";
+    if (dictionary.contains(merchant[i])) {
+      group = dictionary[i];
   }
 }
+  checkGroup(group);
+}
 
-dictionary = ["SPORTING", "ElECTRONIC", "WEAR", "INNS", "FOOD",
-              "RESTAURANTS", "EATING", "RENTAL", "STATIONS", "CREDIT",
-            "COMPUTER", "ELECTRICAL", "RECORD"]
+function checkGroup(group) {
+  var answer;
+  if (["SPORTING", "RECORD", "CREDIT"].contains(group)) {
+    answer = "entertainment";
+  }
+  else if (["RESTAURANTS", "EATING"].contains(group)) {
+    answer = "eating out";
+  }
+  else if (["ELECTRONIC", "COMPUTER"].contains(group)) {
+    answer = "electronics";
+  }
+  else if (["RENTAL", "INNS", "STATIONS"].contains(group)) {
+    answer = "travel";
+  }
+  else if ("FOOD" === group) {
+    answer = "groceries";
+  }
+  else if ("WEAR" === group) {
+    answer = "clothing";
+  }
+  return answer;
+}
+
+//txt += merchant[i] + " ";
+//for (i = 0; i < dictionary.length; i++) {
+//if (txt.search(dictionary[i])) {
+//var group = dictionary[i];
+//}
+//}
